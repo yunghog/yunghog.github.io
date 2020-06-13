06c21a3a9f8282ac32dcd26391bbfe7f6dff3148
Samartha <41548444+yunghog@users.noreply.github.com>
2020-06-13 17:05:09 +0530

    abouot

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,13 +7,14 @@ import '../assets/css/style.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTerminal } from "@fortawesome/free-solid-svg-icons";
 class About extends React.Component{
-fix=()=> {
-      document.getElementById('footer').style.position='fixed';
-      // alert('hello');
+  active=()=>{
+    document.getElementById('about').style.textDecoration('underline');
+    alert('hello');
   }
   render(){
     return (
-          <section className="content-wrap">
+          <section className="content-wrap fadeUp-anim">
+
             <div>
               <Container>
                 <Row>
@@ -22,10 +23,33 @@ fix=()=> {
                   </Col>
                 </Row>
                 <Row>
-                  <Col>
-                    <h2 className="text-xl"><span className="red">I am</span> Samartha</h2>
+                  <Col sm={8}>
+                    <h2 className="text-xl">Myself</h2>
+                    <p className="para">My name is Samartha. Im from Sagar, Shimoga. At present im a 3rd year Information Scince and Engineering student at Jawaharlal Nehru National
+                    College of Engineering, Shimoga which is affilated to VTU. I will graduate by 2021.</p>
                   </Col>
                 </Row>
+                <br/>
+                <Row>
+                  <Col sm={8}>
+                    <h2 className="text-xl">Education</h2>
+                    <div className="edu">
+                      <h5>JNNCE<span> 2017-21</span></h5>
+                      <p>B.E in Information Science and Engineering <br/> Affilated to VTU. <span className="red">CGPA: 7.49</span></p>
+                    </div>
+                    <br/>
+                    <div className="edu">
+                      <h5>Govt. PU Colege<span> 2015-17</span></h5>
+                      <p>Pre University. Science (Physics, Chemistry, Maths & Computer Science ) <br/><span className="red">Percentage: 86.75%</span></p>
+                    </div>
+                    <br/>
+                    <div className="edu">
+                      <h5>Pragathi Composite School<span> 1st to 10th Grade</span></h5>
+                      <p>Karnataka state board.<br/>Passed 10th grade with <span className="red">95%</span></p>
+                    </div>
+                  </Col>
+                </Row>
+                <br/>
               </Container>
             </div>
           </section>
