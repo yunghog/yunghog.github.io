cc246bfebe5b6beb9f248acc8da303e280235430
yunghog <samarthahm@gmail.com>
2020-06-14 17:57:39 +0530

    run to install all the dependencies required by the app

---
+++ installDependency.sh
@@ -0,0 +1,1 @@
+npm install --save @fortawesome/fontawesome-svg-core @fortawesome/free-brands-svg-icons @fortawesome/free-regular-svg-icons @fortawesome/free-solid-svg-icons @fortawesome/react-fontawesome bootstrap react react-bootstrap react-dom react-router-dom react-scripts
