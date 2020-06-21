47427b1007932f6946a666adacc62578f0c1d581
yunghog <samarthahm@gmail.com>
2020-06-21 22:48:41 +0530

    navbar

--- src/assets/components/navBar.js
+++ src/assets/components/navBar.js
@@ -8,36 +8,41 @@ const link_style={
     color: '#eee'
 };
 class Navbar1 extends Component {
-	render() {
+  constructor(props){
+    super(props);
+    this.state = {};
+    this.myHeader = React.createRef();
+}
+ render() {
 		return (
-      <header id={"myHeader"}  ref={ref => this.id = ref} >
-        <Navbar  expand="lg" style={{ width: '100%', backgroundColor: '#e220' }}>
+      <header >
+      <script type="text/javascript">
+      window.onscroll = function() {myFunction()};
+      const header1=this.myHeader.current;
+      const sticky = header1.offsetTop+150;
+      alert(header1.style.display);
+      myFunction=()=>{
+        if (window.pageYOffset > sticky) {
+          header1.classList.add("sticky-header");
+        }else {
+          header1.classList.remove("sticky-header");
+        }
+      }
+      </script>
+        <Navbar  expand="lg" className="navBar" ref={this.myHeader}>
           <Navbar.Brand href="/" className="red"></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav" className="text-right">
             <Nav className="mr-auto ">
               <Nav.Link activeClassName="active"  style={link_style} href="/">home</Nav.Link>
-              <Nav.Link  style={link_style}  activeClassName="active" href="/about">about</Nav.Link>
-              <Nav.Link style={link_style} href="/project">projects</Nav.Link>
+              <Nav.Link  style={link_style}  activeclassname="active" href="/about">about</Nav.Link>
+              <Nav.Link style={link_style} activeclassname="active" href="/project">projects</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
       </header>
 		)
 	}
-  navFix=()=>{
-    // window.onscroll = function() {myFunction()};
-    // var header = document.getElementsByTagName('header')
-    var header =  this.refs.myHeader;
-    // var sticky = header.getBoundingClientRect()+150;
-    alert(this.refs.myHeader);
-    function myFunction() {
-      if (window.pageYOffset > 200) {
-        header.classList.add("sticky-header");
-      }else {
-        header.classList.remove("sticky-header");
-      }
-    }
-  }
+
 }
 export default Navbar1;
