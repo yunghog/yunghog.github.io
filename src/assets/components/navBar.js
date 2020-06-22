317e1734829d187965102e91e5e0d9b2ce137ef7
yunghog <samarthahm@gmail.com>
2020-06-22 20:37:06 +0530

    navbar

--- src/assets/components/navBar.js
+++ src/assets/components/navBar.js
@@ -3,27 +3,25 @@ import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/style.css';
 import { ReactBootstrapStyle,Navbar,Button,Nav,NavDropdown } from 'react-bootstrap';
-const link_style={
-    fontFamily: 'monumental',
-    color: '#eee'
-};
- render() {
-		return (
-      <header >
-        <Navbar  expand="lg" className="navBar" ref={this.myHeader}>
-          <Navbar.Brand href="/" className="red"></Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" className="text-right">
-            <Nav className="mr-auto ">
-              <Nav.Link activeClassName="active"  style={link_style} href="/">home</Nav.Link>
-              <Nav.Link  style={link_style}  activeclassname="active" href="/about">about</Nav.Link>
-              <Nav.Link style={link_style} activeclassname="active" href="/project">projects</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </header>
-		)
-	}
 
+class Navbar1 extends Component{
+   render() {
+  		return (
+        <header >
+          <Navbar  expand="lg" className="navBar" ref={this.myHeader}>
+            <Navbar.Brand href="/" className="red"></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav" className="text-right">
+              <Nav className="mr-auto ">
+                <Nav.Link activeclassname="active" className="navLink" href="/">home</Nav.Link>
+                <Nav.Link activeclassname="active" className="navLink" href="/about">about</Nav.Link>
+                <Nav.Link activeclassname="active" className="navLink" href="/project">projects</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </header>
+  		)
+  	}
 }
+
 export default Navbar1;
