3bdce518d52450fe0ba74945d4f099bac4afa56b
yunghog <samarthahm@gmail.com>
2020-06-23 18:41:24 +0530

    added hobby projects

--- src/pages/project.js
+++ src/pages/project.js
@@ -9,6 +9,8 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import proj1 from '../assets/images/proj1.jpg';
 import proj2 from '../assets/images/proj2.jpg';
 import proj3 from '../assets/images/proj3.jpg';
+import proj4 from '../assets/images/proj4.jpg';
+import proj5 from '../assets/images/proj5.jpg';
 
 class Project extends React.Component {
   constructor(props){
@@ -37,6 +39,7 @@ render() {
   const pi2={
     width:'100%',
   }
+
   return (
     <section>
 
@@ -108,7 +111,29 @@ render() {
               <a target="_blank" href="https://github.com/yunghog/spotify-adblock"><button className="btn btn-1"><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon> view</button></a>
               </div>
             </div></Col>
+            <Col xl={4} xs={12}><div className="proj2">
+              <div className="proj-img2"><Image src={proj4} style={pi2}/></div>
+              <div className="proj-desc2"><h5>Host Editor</h5>
+              <p>Python CLI application to block ads and sites by adding domains into hosts</p>
+              <p>Built using <span className="red">python 3.7</span></p>
+              <a target="_blank" href="https://github.com/yunghog/host-editor"><button className="btn btn-1"><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon> view</button></a>
+              </div>
+            </div></Col>
+            <Col xl={4} xs={12}><div className="proj2">
+              <div className="proj-img2"><Image src={proj5} style={pi2}/></div>
+              <div className="proj-desc2"><h5>Dealership Management System (DMS)</h5>
+              <p>Java Swings application which can be implemented in an Automobile Workshops
+              to maintain and manipulate the records</p>
+            <p>Built using <span className="red">JAVA</span></p>
+              <a target="_blank" href="https://github.com/yunghog/Dealership-Management-System"><button className="btn btn-1"><FontAwesomeIcon icon={faGithub}></FontAwesomeIcon> view</button></a>
+              </div>
+            </div></Col>
           </Row>
+          <br/>
+          <Row><Col><h3 className="text-xl">Graphic Design</h3></Col></Row>
+          <Row><Col><div>
+
+          </div></Col></Row>
         </Container>
         <br/><br/>
     </section>
