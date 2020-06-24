2da8f194dcd2f66341763249a0535ba357f5880f
yunghog <samarthahm@gmail.com>
2020-06-24 13:29:21 +0530

    Updates

--- installDependency.sh
+++
@@ -1,2 +0,0 @@
-npm install --save @fortawesome/fontawesome-svg-core @fortawesome/free-brands-svg-icons @fortawesome/free-regular-svg-icons @fortawesome/free-solid-svg-icons @fortawesome/react-fontawesome bootstrap react react-bootstrap react-dom react-router-dom react-scripts
-#if you use windows, just copy the above line and execute in command prompt hahaha
