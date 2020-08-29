eb9664f2f57292b086998687b84b890bd1a91b99
yunghog <samarthahm@gmail.com>
2020-08-29 22:13:26 +0530

    added aditya automobiles project details

--- src/pages/project.js
+++ src/pages/project.js
@@ -9,7 +9,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faReact, faNodeJs, faBootstrap, faJs, faPhp} from "@fortawesome/free-brands-svg-icons";
 import ms800hub from '../assets/images/projects/ms800hub.jpg';
 import ms800hubMock from '../assets/images/projects/ms800hubMockup.png';
-
+import adityaAuto from '../assets/images/projects/adityaAuto.jpg';
+import adityaAutoMockup from '../assets/images/projects/adityaAutoMockup.png';
 class Project extends React.Component {
   constructor(props){
     super(props);
@@ -83,6 +84,32 @@ render() {
                   <Col md={5}>
                     <div className="proj-mockup cell-right">
                       <Image src={ms800hubMock}/>
+                    </div>
+                  </Col>
+                </Row>
+              </div>
+              <div>
+                <Row noGutters>
+                  <Col md={7}>
+                    <div className="proj-desc-con">
+                      <Image src={adityaAuto}/>
+                      <div className="proj-desc cardy inverse">
+                        <h3>Aditya Automobiles</h3>
+                        <p>Website | LAMP Stack</p>
+                        <p>
+                          MS800 Hub is an automotive club website. It is a small social media for car
+                           enthusiast. Car enthusiasts can sign up and post pictures of cars, post articles
+                           or their build details in blog section.
+                        </p>
+                        <p><FontAwesomeIcon style={icon} icon={faBootstrap}></FontAwesomeIcon>
+                        <FontAwesomeIcon style={icon} icon={faJs}></FontAwesomeIcon>
+                      <FontAwesomeIcon style={icon} icon={faPhp}></FontAwesomeIcon></p>
+                      </div>
+                    </div>
+                  </Col>
+                  <Col md={5}>
+                    <div className="proj-mockup cell-right">
+                      <Image src={adityaAutoMockup}/>
                     </div>
                   </Col>
                 </Row>
