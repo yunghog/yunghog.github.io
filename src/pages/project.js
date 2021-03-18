7e85116aa44ba409cef7d3b53e84da30fea332cf
Samartha <samarthahm@gmail.com>
2021-03-18 18:51:06 +0530

    project page initi

--- src/pages/project.js
+++ src/pages/project.js
@@ -1,45 +1,37 @@
-import React, {Component} from "react";
-import { Container,Row,Col, Button, Image } from 'react-bootstrap';
-import AOS from "aos";
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import React from 'react';
+import { Container,Row,Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../assets/css/style.css'
+import ReactDOM from 'react-dom';
+import AOS from 'aos';
+import '../assets/css/style.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faReact, faNodeJs, faBootstrap, faJs, faPhp} from "@fortawesome/free-brands-svg-icons";
-class Project extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {};
-}
+import { faCss3, faAngular, faHtml5,  faReact, faNodeJs, faBootstrap, faJs, faPhp, faGitAlt, faBitbucket, faInstagram, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope ,faAtom } from "@fortawesome/free-solid-svg-icons";
 
-render() {
-  AOS.init({
-    duration: 1000,
-    once: true
-});
-  const icon = {
-    fontSize: '42px',
-    padding: '5px'
-  };
-  return (
-    <section>
-      <Container>
-        <Row>
-          <Col data-aos="fade-up">
-            <h1 className="text-xxl"> Projects<span className="red">.</span></h1>
-          </Col>
-        </Row>
-      </Container>
-      <br/>
-      <Container>
-        <Row>
-          <Col>
-          </Col>
-        </Row>
-      </Container>
-    </section>
-  );
+class About extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state={ };
+  }
+  render(){
+    AOS.init({
+      duration: 1000,
+      once: true
+    });
+
+    return (
+      <section className="content-wrap fadeUp-anim">
+        <div>
+          <Container>
+            <Row>
+              <Col data-aos="fade-up">
+                <h1 className="text-xxl">Works<span className="red">.</span></h1>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </section>
+    );
+  }
 }
-}
-export default Project;
+export default About;
