10d779274dbab28bc43b815880fe459001c76fce
Samartha <samarthahm@gmail.com>
2021-03-18 18:40:35 +0530

    clean up

---
+++ src/assets/components/proj-images.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+import { Container,Row,Col, Button, Image } from 'react-bootstrap';
+import AOS from "aos";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/style.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDatabase } from "@fortawesome/free-solid-svg-icons";
+import {faReact, faNodeJs, faBootstrap, faJs, faPhp, faJava, faPython} from "@fortawesome/free-brands-svg-icons";
+import ms800hub from '../images/projects/ms800hub.jpg';
+import ms800hubp from '../images/projects/ms800hubp.jpg';
+import ms800hubMock from '../images/projects/ms800hubMockup.png';
+import adityaAuto from '../images/projects/adityaAuto.jpg';
+import adityaAutop from '../images/projects/adityaautomobilesp.jpg';
+import adityaAutoMockup from '../images/projects/adityaAutoMockup.png';
+import linkTree from '../images/projects/linkTree.jpg';
+import linkTreeMockup from '../images/projects/linkTreeMockup.png';
