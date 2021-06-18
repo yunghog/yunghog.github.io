4e4973c310a80d5c183ef558aae5f096e86a3201
Samartha <samarthahm@gmail.com>
2021-06-18 13:07:09 +0530

    new branch init

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
 import React, { Component } from 'react';
 import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
 
-import Header from './assets/components/header';
-import Footer from './assets/components/footer';
-
 import Home from './pages/home';
-import About from './pages/about';
-import Project from './pages/project';
-import Resume from './pages/resume';
-import CaseStudy from  './pages/case-study';
-import NotFound from './pages/not-found';
 
 
 class App extends Component {
   render() {
     return (
        <HashRouter>
-          <Header/>
             <Switch>
              <Route exact path="/"><Home/></Route>
-             <Route path="/about"><About/></Route>
-             <Route exact path="/project"><Project/></Route>
-             <Route path="/resume"><Resume/></Route>
-             <Route path="/project/case_study/:proj" component={CaseStudy}/>
-             <Route path="/oops" component={NotFound} />
-             <Redirect  from="*" to="/oops" />
            </Switch>
-           <Footer/>
       </HashRouter>
     );
   }
